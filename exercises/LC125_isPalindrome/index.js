b1cac269495a78daae97bf494fc515480a3c63e1
Andy Chhuon <chhuonandy@gmail.com>
2021-12-20 21:43:18 -0500

    Done LC#125 and LC#5

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,15 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  s = s.toLowerCase().replace(/[\W_]/g, "");
+  let pointer1 = 0;
+  let pointer2 = s.length - 1;
+  while (pointer1 < pointer2) {
+    if (s.charAt(pointer1) != s.charAt(pointer2)) {
+      return false;
+    }
+    pointer1++;
+    pointer2--;
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,34 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  let biggestLength = 1;
+  let palindromePos = 0; //First char of palindrome
+
+  let palindromeAroundMiddle = (pointer1, pointer2) => {
+    console.log(pointer1);
+    while (
+      s[pointer1] == s[pointer2] &&
+      s[pointer1] != undefined &&
+      s[pointer2] != undefined
+    ) {
+      if (pointer2 - pointer1 + 1 > biggestLength) {
+        biggestLength = pointer2 - pointer1 + 1;
+        palindromePos = pointer1;
+        console.log(palindromePos);
+      }
+      pointer1--;
+      pointer2++;
+    }
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    palindromeAroundMiddle(i, i + 1);
+    palindromeAroundMiddle(i - 1, i + 1);
+  }
+  console.log(palindromePos);
+  console.log(biggestLength);
+  return s.substring(palindromePos, palindromePos + biggestLength);
+}
 
 module.exports = longestPalindrome;
