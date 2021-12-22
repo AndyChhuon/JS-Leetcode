76f64059cf693efd9e42ffc9b485072094849974
Andy Chhuon <chhuonandy@gmail.com>
2021-12-21 21:06:43 -0500

    Done LC#3

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,23 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  let charObj = {};
+  let startIndex = 0;
+  let maxLength = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let lastChar = s[i];
+    if (charObj[lastChar] != undefined) {
+      startIndex = charObj[lastChar] + 1;
+    }
+
+    charObj[lastChar] = i;
+
+    maxLength = Math.max(maxLength, i - startIndex + 1);
+  }
+
+  return maxLength;
+}
 
 module.exports = lengthOfLongestSubstring;
